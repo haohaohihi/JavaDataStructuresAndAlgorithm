0e26a28c13d5bc4ca3af1b918d5cbddbbea939f7
haohaohihi <haohaohihi@gmail.com>
2018-04-14 20:40:45 +0800

    完成二叉查找树的实现

--- examples/Chapter4/Tree/BinarySearchTree.java
+++ examples/Chapter4/Tree/BinarySearchTree.java
@@ -69,23 +69,66 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 
 
     private boolean contains(T x, BinaryNode<T> t) {
-        return false;
+        if (t == null) {
+            return false;
+        }
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            return contains(x, t.left);
+        } else if (compareResult > 0) {
+            return contains(x, t.right);
+        } else {
+            return true;
+        }
     }
-
     private BinaryNode<T> findMin(BinaryNode<T> t) {
-        return null;
+        if (t != null) {
+            while (t.left != null) {
+                t = t.left;
+            }
+        }
+        return t;
     }
 
     private BinaryNode<T> findMax(BinaryNode<T> t) {
-        return null;
+        if (t != null) {
+            while (t.right != null) {
+                t = t.right;
+            }
+        }
+        return t;
     }
 
     private BinaryNode<T> insert(T x, BinaryNode<T> t) {
-        return null;
+        if (t == null) {
+            return new BinaryNode<T>(x, null, null);
+        }
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = insert(x, t.left);
+        } else if (compareResult > 0) {
+            t.right = insert(x, t.right);
+        }
+        return t;
     }
 
     private BinaryNode<T> remove(T x, BinaryNode<T> t) {
-        return null;
+        if (t == null) {
+            return t;
+        }
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = remove(x, t.left);
+        } else if (compareResult > 0) {
+            t.right = remove(x, t.right);
+        } else if (t.left != null && t.right != null){
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element, t.right);
+        } else {
+            t = (t.left != null) ? t.left : t.right;
+        }
+        return t;
+
     }
 
     private void printTree(BinaryNode<T> t) {
